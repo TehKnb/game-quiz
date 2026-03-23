603b0db81bef5a9e45cbf509dbad8641919727dd
wasabik wasabik <biznesclubknb@gmail.com>
2026-03-23 21:00:05 +0200

    new test 15

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Page() {
   return (
     <main className="min-h-screen bg-[#f5f5f5] px-4 py-8">
       <div className="mx-auto w-full max-w-[920px]">
-        <div className="fixed top-[5px] left-1/2 z-50 w-full max-w-[920px] -translate-x-1/2 px-4">
+        <div className="fixed top-[2px] left-1/2 z-50 w-full max-w-[920px] -translate-x-1/2 px-4">
           <QuizHeader
             currentQuestion={currentQuestionNumber}
             totalQuestions={totalQuestions}
@@ -70,7 +70,7 @@ export default function Page() {
           />
         </div>
 
-        <div className="pt-[90px]">
+        <div className="pt-[25px]">
           <QuizQuestionCard
             totalQuestions={totalQuestions}
             questionData={currentQuestion}
